54d552df5276bd9fea0b442f980d7fb84fcaf04d
Sreepathi Pai <sree@cs.rochester.edu>
2022-08-25 13:06:26 -0400

    Generate complete include file instead of .h/.c for int

--- int_sampler.py
+++ int_sampler.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from pattern import BitPattern as BP
+from pattern import BitPattern as BP, generate_sampler
 
 MIN = BP('1'*32)
 ZERO = BP('0'*32)
@@ -18,6 +18,13 @@ decoders = [('int32_min', MIN),
             ('int32_pos', POS),
             ('int32_max', MAX)]
 
+print("#pragma once")
+print("#include <stdint.h>")
+print("#include <stdio.h>")
+print("#include <assert.h>")
+print("#include <math.h>")
+print("#include <stdlib.h>")
+
 count = 0
 for n, d in decoders:
     print(f"static uint32_t {n}_range = {d.count()};")
@@ -27,3 +34,6 @@ for n, d in decoders:
 gen_count = count
 tot_count = 2**32
 assert gen_count == tot_count, f"{gen_count} == {tot_count}"
+
+print(generate_sampler("sample_int32", [d[0] for d in decoders],
+                       value_ty = "int32_t"))
